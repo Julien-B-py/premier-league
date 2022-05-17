7b5d69c82c9806daef3f455788463b3afaf20bae
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-05-17 17:13:59 +0200

    v1.4.1
    Add conditional display to searchbar.
    Edit CSS.

--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,37 @@
+const iconsDiv = document.querySelector(".nav__icons");
+const searchButton = document.querySelector(".fa-magnifying-glass");
+const closeButton = document.querySelector(".fa-xmark");
+const searchDiv = document.querySelector(".nav__search");
 const searchInput = document.getElementById("club-search");
 const cardsContainer = document.querySelector(".club__cards");
 
+searchInput.value = "";
+
 const cardsCopy = [...cards];
 
+const setSearchInputVisible = () => {
+    if (window.getComputedStyle(searchDiv).display === "none") {
+        searchDiv.style.display = 'flex';
+        iconsDiv.style.display = 'none';
+        searchInput.focus();
+    }
+}
+
+const setSearchInputHidden = () => {
+    if (window.getComputedStyle(searchDiv).display === "flex") {
+        searchDiv.style.display = 'none';
+        iconsDiv.style.display = 'flex';
+        // Clear search input
+        searchInput.value = "";
+        // Reset display
+        cardsCopy.forEach(card => cardsContainer.appendChild(card));
+
+    }
+}
+
+closeButton.addEventListener("click", setSearchInputHidden);
+
+searchButton.addEventListener("click", setSearchInputVisible);
 
 searchInput.addEventListener('input', (event) => {
 
